ff730086b76f347e81461aaed77076cc50c5237b
JMSBPP <jmsbpp@hotmail.es>
2025-11-11 11:57:12 -0500

    chore/updated dependencies

---
+++ test/ForkTest.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Test, console2} from "forge-std/Test.sol";
+
+import "./fork/ForkUtils.sol";
+
+contract ForkTest is Test{
+    uint256 unichain_fork;
+    uint256 reactive_fork;
+
+
+
+
+    address user;
+    function setUp() public virtual{
+        uint256 deployer_private_key = vm.envUint("PRIVATE_KEY");
+        user = vm.addr(deployer_private_key);
+
+        reactive_fork = vm.createFork(vm.envString("REACTIVE_RPC_ENDPOINT"));
+        unichain_fork = vm.createFork(vm.envString("UNICHAIN_RPC_ENDPOINT"));
+
+    }
+
+    function pre_deploy_reactive(address _deployer) internal returns(bytes32){
+        vm.selectFork(reactive_fork);
+        assertEq(block.chainid , REACTIVE_CHAIN_ID);
+        vm.deal(_deployer, REACT_AMOUNT_TO_DEPLOY);
+        
+        bytes32 reactive_salt = keccak256(abi.encodePacked("reactive", REACTIVE_CHAIN_ID));
+        return reactive_salt;
+    }
+       
+
+    function pre_deploy_unichain(address _deployer) internal returns(bytes32,bytes32){
+        vm.selectFork(unichain_fork);
+        assertEq(block.chainid, UNICHAIN_CHAIN_ID);
+        vm.deal(_deployer, ETHER_AMOUNT_TO_DEPLOY);
+        bytes32 unichain_salt_endpoint = keccak256(abi.encodePacked("unichain-endpoint", UNICHAIN_CHAIN_ID));
+        bytes32 unichain_salt_listener = keccak256(abi.encodePacked("unichain-listener", UNICHAIN_CHAIN_ID));
+        return (unichain_salt_endpoint,unichain_salt_listener);
+    }
+
+
+
+
+}
---
+++ test/fork/ForkUtils.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+
+// address constant SWAP_ROUTER = address(0xef740bf23acae26f6492b10de645d6b98dc8eaf3);
+// address constant ETH_WHALE = address(0x9509b2033670F4faCC043C1D80Ccf324e639B4b5);
+
+// uint256 constant ANY_LIQUIDITY_POSITION_TOKEN_ID = uint256(2_000_000);
+// // We want to know the starting block.number, or time where this position was born
+
+
+////==================================UNICHAIN========================================
+uint256 constant UNICHAIN_CHAIN_ID = 130;
+uint256 constant ETHER_AMOUNT_TO_DEPLOY = 1 ether;
+address constant UNIVERSAL_ROUTER = address(0xEf740bf23aCaE26f6492B10de645D6B98dC8Eaf3);
+address constant QUOTER_V4 = address(0x333E3C607B141b18fF6de9f258db6e77fE7491E0);
+address constant POSITION_MANAGER = address(0x4529A01c7A0410167c5740C487A8DE60232617bf);
+address constant PERMIT2 = address(0x000000000022D473030F116dDEE9F6B43aC78BA3);
+address constant UNICHAIN_CALLBACK_PROXY_ADDRESS = address(0x9299472A6399Fd1027ebF067571Eb3e3D7837FC4);
+address constant USDC = address(0x078D782b760474a361dDA0AF3839290b0EF57AD6);
+address constant ETH = address(0x00);
+
+
+//====================================SEPOLIA========================================
+uint256 constant SEPOLIA_CHAIN_ID = 11155111;
+address constant SEPOLIA_CALLBACK_PROXY_ADDRESS = address(0xc9f36411C9897e7F959D99ffca2a0Ba7ee0D7bDA);
+
+//==============================REACTIVE==============================================
+uint256 constant REACTIVE_CHAIN_ID = 1597;
+uint256 constant REACT_AMOUNT_TO_DEPLOY = 20 ether; // 20 react
+address constant SYSTEM_CONTRACT = address(0x0000000000000000000000000000000000fffFfF);
+address constant NODE = address(0x64);
+//=================================LASNA============================================
+uint256 constant LASNA_CHAIN_ID = 5318007;
+
+
+
