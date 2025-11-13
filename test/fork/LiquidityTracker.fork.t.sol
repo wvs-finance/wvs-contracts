82b5d0a3d2967eaf11b9f297f641315b2ce7e061
JMSBPP <jmsbpp@hotmail.es>
2025-11-13 18:30:40 -0500

    Revert "feat: implement liquidity minting via Universal Router in fork tests (#2)"
    This reverts commit c9889d5ea762e1ad93dccce51e7ab81ca86d4c6e.

--- test/fork/LiquidityTracker.fork.t.sol
+++ test/fork/LiquidityTracker.fork.t.sol
@@ -1,118 +1,118 @@
-// // SPDX-License-Identifier: GPL-3.0-or-later
-// pragma solidity ^0.8.17;
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
 
 
-// import "./unichain/ForkUtils.sol";
-// import {console2} from "forge-std/console2.sol";
-// import {Test} from "forge-std/Test.sol";
+import "./unichain/ForkUtils.sol";
+import {console2} from "forge-std/console2.sol";
+import {Test} from "forge-std/Test.sol";
 
-// // The goal is to track all the information about a position
-// // on Uniswap using a large position holder as a starting point
+// The goal is to track all the information about a position
+// on Uniswap using a large position holder as a starting point
 
-// // 1. Let's get the tokenId of a position and based on that we get the
-// // liquidity position and can track all since it's starting point
+// 1. Let's get the tokenId of a position and based on that we get the
+// liquidity position and can track all since it's starting point
 
-// // We want to know the starting block.number,
-// // or time where this position was born
+// We want to know the starting block.number,
+// or time where this position was born
 
-// abstract contract ForkHelper{
-
-
-//     struct fork_metadata{
-//         string rpc_endpoint_api_key;
-//         string network_name;
-//     }
+abstract contract ForkHelper{
 
 
-//     mapping (uint256 _forkId => bool _init_) fork_initiated;
+    struct fork_metadata{
+        string rpc_endpoint_api_key;
+        string network_name;
+    }
 
-//     modifier only_fork_initiated(uint256 _fork_id){
-//         if (!fork_initiated[_fork_id]) revert("fork not initiated");
-//         _;
-//     }
+
+    mapping (uint256 _forkId => bool _init_) fork_initiated;
+
+    modifier only_fork_initiated(uint256 _fork_id){
+        if (!fork_initiated[_fork_id]) revert("fork not initiated");
+        _;
+    }
     
-//     function fork_init(fork_metadata calldata _fork_metadata ) public returns (uint256 _fork_id){
-//         try vm.envString(_fork_metadata.rpc_endpoint_api_key) returns (string memory){
-//             uint256 forkId = vm.createFork(
-//                 vm.rpcUrl(_fork_metadata.network_name)
-//             );
+    function fork_init(fork_metadata calldata _fork_metadata ) public returns (uint256 _fork_id){
+        try vm.envString(_fork_metadata.rpc_endpoint_api_key) returns (string memory){
+            uint256 forkId = vm.createFork(
+                vm.rpcUrl(_fork_metadata.network_name)
+            );
 
-//             fork_initiated[forkId] = true;
+            fork_initiated[forkId] = true;
 
-//         } catch {
-//             fork_initiated[forkId] = false;
-//             console2.log("Failed on fork");
-//         }
+        } catch {
+            fork_initiated[forkId] = false;
+            console2.log("Failed on fork");
+        }
 
-//     }
-
-    
+    }
 
     
-//     function _findLiquidityPositionStartingBlockNumber(
-//         uint256 _forkId,
-//         uint256 _tokenId,
-//         uint256 _seed,
-//         fork_metadata calldata _fork_metadata
-//     ) internal only_fork_initiated(_forkId) returns (uint256 startingBlockNumber){
 
-//     }
+    
+    function _findLiquidityPositionStartingBlockNumber(
+        uint256 _forkId,
+        uint256 _tokenId,
+        uint256 _seed,
+        fork_metadata calldata _fork_metadata
+    ) internal only_fork_initiated(_forkId) returns (uint256 startingBlockNumber){
+
+    }
     
 
-// }
+}
 
-// contract LiquidityPositionTrackerTracker is Test, ForkHelper{
+contract LiquidityPositionTrackerTracker is Test, ForkHelper{
     
 
-//     uint48 constant ONGOING_POSITION_SENTINEL = uint48(0xffffffffffff);
+    uint48 constant ONGOING_POSITION_SENTINEL = uint48(0xffffffffffff);
 
 
-//     struct FeeRevenue{
-//         uint256 feeGrowthInside0LastX128;
-//         uint256 feeGrowthInside1LastX128;
-//     }
+    struct FeeRevenue{
+        uint256 feeGrowthInside0LastX128;
+        uint256 feeGrowthInside1LastX128;
+    }
 
 
-//     struct All_liquidity{
-//         uint48 startingBlock;
-//         uint256 positionInfo;
-//         FeeRevenue fee_revenue;
+    struct All_liquidity{
+        uint48 startingBlock;
+        uint256 positionInfo;
+        FeeRevenue fee_revenue;
        
-//     }
-//     // NOTE: This uses the uniswapFoundation/foundationalHooks
-//     // for conversion to uint160
-//     struct Pricing{
-//         uint160 external_price_token0;
-//         uint160 external_price_token1
-//         uint160 internal_price_token0;
-//         uint260 internal_price_token1;
-//     }
+    }
+    // NOTE: This uses the uniswapFoundation/foundationalHooks
+    // for conversion to uint160
+    struct Pricing{
+        uint160 external_price_token0;
+        uint160 external_price_token1
+        uint160 internal_price_token0;
+        uint260 internal_price_token1;
+    }
 
-//     // TODO: What library helps get this amounts
-//     // reliably
-//     struct LiquidityPerToken{
-//         uint256 liquidityOnToken0;
-//         uint256 liquidityOnToken1;
-//     }
+    // TODO: What library helps get this amounts
+    // reliably
+    struct LiquidityPerToken{
+        uint256 liquidityOnToken0;
+        uint256 liquidityOnToken1;
+    }
 
-//     // TODO: Define events for liquidity managent
-//     event ImpermanentLoss();
-//     event DivergenceLoss();
-//     // TODO: These are to be heard
+    // TODO: Define events for liquidity managent
+    event ImpermanentLoss();
+    event DivergenceLoss();
+    // TODO: These are to be heard
 
-//     mapping(bytes32 poolId => mapping(uint256 liquidity_token_id => All_liquidity)) liquidity_position_intel;
+    mapping(bytes32 poolId => mapping(uint256 liquidity_token_id => All_liquidity)) liquidity_position_intel;
 
 
 
-//     function setUp() public {
+    function setUp() public {
 
-//     }
+    }
 
 
 
 
     
-// }
+}
 
 
 
---
+++ test/fork/unichain/ForkUtils.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.17;
+
+
+address constant LIQUIDITY_POSITION_MANAGER = address(0x4529A01c7A0410167c5740C487A8DE60232617bf);
+address constant SWAP_ROUTER = address(0xef740bf23acae26f6492b10de645d6b98dc8eaf3);
+address constant ETH_WHALE = address(0x9509b2033670F4faCC043C1D80Ccf324e639B4b5);
+
+uint256 constant ANY_LIQUIDITY_POSITION_TOKEN_ID = uint256(2_000_000);
+// We want to know the starting block.number, or time where this position was born
+
+
+
